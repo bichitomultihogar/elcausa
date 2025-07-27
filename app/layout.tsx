ff4f6c0a2795664c19800b2d6104c5567bfda108
brianGelvez <briannn97@gmail.com>
2025-07-27 18:32:40 -0300

    favicon3

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 }
 
+
 export default function RootLayout({
   children,
 }: {
